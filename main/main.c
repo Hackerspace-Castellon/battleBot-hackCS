e2652aa73834223ab842fc1fea2511d40a74ca10
vik0t0r <al404303@uji.es>
2024-08-14 17:42:44 +0200

    correctly printing

--- main/main.c
+++ main/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <esp_log.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static const char *TAG = "MAIN";
 
 void app_main(void)
 {
+    ESP_LOGI(TAG, "Starting main app");
+    int counter = 0;
 
+    for (;;){
+        ESP_LOGI(TAG, "waiting: %d", counter);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        counter++;
+    } 
 }
