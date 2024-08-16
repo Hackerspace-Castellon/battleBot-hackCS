b6ae3c29502d18826f279f63a334d9052a9c4d92
vik0t0r <al404303@uji.es>
2024-08-16 13:37:54 +0200

    setup ledc pwm

--- main/main.c
+++ main/main.c
@@ -14,10 +14,20 @@
 #include <uni.h>
 
 #include "sdkconfig.h"
+#include <driver/ledc.h>
 
 // Defined in my_platform.c
 struct uni_platform* get_my_platform(void);
 
+// define motors
+#define MOTOR_DELANTERO_DERECHO 0
+#define MOTOR_DELANTERO_IZQUIERDO 1
+#define MOTOR_TRASERO_DERECHO 2
+#define MOTOR_TRASERO_IZQUIERDO 3
+
+// forward
+#define DIRECTION_FORWARD 0
+#define DIRECTION_BACKWARDS 1
 
 // motor delantero derecho
 #define PIN_MDD1 14
@@ -35,7 +45,9 @@ struct uni_platform* get_my_platform(void);
 #define PIN_MTI1 25
 #define PIN_MTI2 26
 
-// estas funciónes hay que pasarle el pin1 y pin2 del motor que queremos que avance hacia adelante.
+// estas funciónes hay que pasarle el pin1 y pin2 del motor que queremos que avance hacia adelante. 
+
+// SIN PWM
 
 void parada_lenta(gpio_num_t pin1, gpio_num_t pin2){
     gpio_set_level(pin1, 0);
@@ -119,48 +131,153 @@ static const char *TAG = "MAIN";
 static const char * controller_addr_string = "40:8E:2C:63:4F:34";
 
 
+void ledc_init(){
+
+    // usamos mismo timer para todos los 8 PWM
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_HIGH_SPEED_MODE,
+        .duty_resolution  = LEDC_TIMER_10_BIT,
+        .timer_num        = LEDC_TIMER_0,
+        .freq_hz          = 4000,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // preparar motor delantero izquierdo
+
+    // MDI1
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_HIGH_SPEED_MODE,
+        .channel        = LEDC_CHANNEL_0,
+        .timer_sel      = LEDC_TIMER_0,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = PIN_MDI1,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    // MDI2
+    ledc_channel.gpio_num = PIN_MDI2;
+    ledc_channel.channel = LEDC_CHANNEL_1;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    // MDD1
+    ledc_channel.gpio_num = PIN_MDD1;
+    ledc_channel.channel = LEDC_CHANNEL_2;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+    // MDD2
+    ledc_channel.gpio_num = PIN_MDD1;
+    ledc_channel.channel = LEDC_CHANNEL_3;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+
+    //MTD1
+    ledc_channel.gpio_num = PIN_MTD1;
+    ledc_channel.channel = LEDC_CHANNEL_4;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+    //MTD2
+    ledc_channel.gpio_num = PIN_MTD2;
+    ledc_channel.channel = LEDC_CHANNEL_5;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    // MTI1
+    ledc_channel.gpio_num = PIN_MTI1;
+    ledc_channel.channel = LEDC_CHANNEL_6;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+    // MTI2
+    ledc_channel.gpio_num = PIN_MTI2;
+    ledc_channel.channel = LEDC_CHANNEL_7;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+}
+/*
+    @param motor: MOTOR_DELANTERO_DERECHO, MOTOR_DELANTERO_IZQUIERDO, MOTOR_TRASERO_DERECHO, MOTOR_TRASERO_IZQUIERDO
+    @param direction: DIRECTION_FORWARD, DIRECTION_BACKWARDS
+    @param pwm_value: 2¹⁰ = 0-1024 range 
+*/
+
+void mover_motor(uint32_t motor, uint32_t direction ,uint32_t pwm_value){
+
+    ledc_channel_t ch1;
+    ledc_channel_t ch2;
+
+    switch (motor){
+        case MOTOR_DELANTERO_DERECHO:
+            ch1 = 0;
+            ch2 = 0;
+        break;
+        case MOTOR_DELANTERO_IZQUIERDO:
+            ch1 = LEDC_CHANNEL_0;
+            ch2 = LEDC_CHANNEL_1;
+        break;
+        case MOTOR_TRASERO_DERECHO:
+            ch1 = 0;
+            ch2 = 0;
+        break;
+        case MOTOR_TRASERO_IZQUIERDO:
+            ch1 = 0;
+            ch2 = 0;
+        break;
+    }
+
+
+    if (direction == DIRECTION_FORWARD){
+
+        // set pin2
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, ch2, 0);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, ch2);
+
+        // set pin1
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, ch1, pwm_value);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, ch1);
+
+    } else { // DIRECTION_BACKWARDS
+        // set pin1
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, ch1, 0);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, ch1);
+
+        // set pin2
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, ch2, pwm_value);
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, ch2);
+    }
+
+}
+
 void app_main(void)
 {
     ESP_LOGI(TAG, "Starting main app");
-    // pin set up as output
-        uint64_t pin_mask = (1ULL << PIN_MDD1) |
-                        (1ULL << PIN_MDD2) |
-                        (1ULL << PIN_MDI1) |
-                        (1ULL << PIN_MDI2) |
-                        (1ULL << PIN_MTD1) |
-                        (1ULL << PIN_MTD2) |
-                        (1ULL << PIN_MTI1) |
-                        (1ULL << PIN_MTI2);
-
-    // Configure all 8 pins as output
-    gpio_config_t io_conf = {
-        .pin_bit_mask = pin_mask,
-        .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE
-    };
-    gpio_config(&io_conf);
-
-    gpio_set_level(PIN_MDD1, 0);
-    gpio_set_level(PIN_MDD2, 0);
-
-    gpio_set_level(PIN_MDI1, 0);
-    gpio_set_level(PIN_MDI2, 0);
-
-    gpio_set_level(PIN_MTD1, 0);
-    gpio_set_level(PIN_MTD2, 0);
-
-    gpio_set_level(PIN_MTI1, 0);
-    gpio_set_level(PIN_MTI2, 0);
-
     // setup bluetooth
     bd_addr_t controller_addr;
     sscanf_bd_addr(controller_addr_string, controller_addr);
 
+    // set up PWM settings
+    ledc_init();
 
     ESP_LOGI(TAG, "Finished setup");
 
+
+    /*for (;;){
+        avanzar(PIN_MDD1, PIN_MDD2);
+        // avanzar
+            ESP_LOGI(TAG, "AVANZAR");
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 8192));
+        // Update duty to apply the new value
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+        gpio_set_level(PIN_MDD2, 0);
+        vTaskDelay(pdMS_TO_TICKS(5000));
+
+            ESP_LOGI(TAG, "PARAR");
+        // parada brusca
+            ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 0));
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+        gpio_set_level(PIN_MDD2, 0);
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+
+
+    return;*/
 
     // Configure BTstack for ESP32 VHCI Controller
     btstack_init();
@@ -178,6 +295,7 @@ void app_main(void)
     uni_bt_allowlist_add_addr(controller_addr);
     uni_bt_allowlist_set_enabled(true);
     //uni_bt_enable_new_connections_safe(false);
+
 
 
     // Does not return.
